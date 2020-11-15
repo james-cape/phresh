6c472fea8db5338dce75313d089934f3a737843a
James Cape <james.cape@gmail.com>
2020-11-15 07:33:37 -0700

    Adds tests

---
+++ backend/tests/test_cleanings.py
@@ -0,0 +1,19 @@
+import pytest
+
+from httpx import AsyncClient
+from fastapi import FastAPI
+
+from starlette.status import HTTP_404_NOT_FOUND  # TODO: swap starlette for fastapi
+from starlette.status import HTTP_422_UNPROCESSABLE_ENTITY  # TODO: swap starlette for fastapi
+
+
+class TestCleaningsRoutes:
+    @pytest.mark.asyncio
+    async def test_routes_exist(self, app: FastAPI, client: AsyncClient) -> None:
+        res = await client.post(app.url_path_for('cleanings:create-cleaning'), json={})
+        assert res.status_code != HTTP_404_NOT_FOUND
+
+    @pytest.mark.asyncio
+    async def test_invalid_input_raises_error(self, app: FastAPI, client: AsyncClient) -> None:
+        res = await client.post(app.url_path_for('cleanings:create-cleaning'), json={})
+        assert res.status_code != HTTP_422_UNPROCESSABLE_ENTITY
