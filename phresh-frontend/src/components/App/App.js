8f7f1ae22a2b12725bbc14796be255f4b5dcbf23
James Cape <james.cape@gmail.com>
2021-04-20 22:00:43 -0600

    Adds landing page svg

--- phresh-frontend/src/components/App/App.js
+++ phresh-frontend/src/components/App/App.js
@@ -1,13 +1,11 @@
 import React from "react"
-import { Layout } from "../../components"
+import { LandingPage, Layout } from "../../components"
 import { EuiText } from "@elastic/eui"
 
 export default function App() {
   return (
     <Layout>
-      <EuiText>
-        <h1>Phresh</h1>
-      </EuiText>
+      <LandingPage />
     </Layout>
   )
 }
--- phresh-frontend/src/components/index.js
+++ phresh-frontend/src/components/index.js
@@ -1,4 +1,5 @@
 export { default as App } from './App/App'
+export { default as LandingPage } from './LandingPage/LandingPage'
 export { default as Layout } from './Layout/Layout'
 export { default as Navbar } from './Navbar/Navbar'
 
