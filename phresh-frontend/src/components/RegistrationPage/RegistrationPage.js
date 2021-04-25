0273f917bb62d0119cee185a381d3a6b4970380f
James Cape <james.cape@gmail.com>
2021-04-25 15:58:18 -0600

    Adds frontend for login and registration

--- phresh-frontend/src/components/RegistrationPage/RegistrationPage.js
+++ phresh-frontend/src/components/RegistrationPage/RegistrationPage.js
@@ -1,5 +1,55 @@
+// import React from "react"
+
+// export default function RegistrationPage() {
+//   return <h1>Registration</h1>
+// }
+
+
 import React from "react"
+import {
+  EuiPage,
+  EuiPageBody,
+  EuiPageContent,
+  EuiPageContentBody,
+  EuiPageHeader,
+  EuiPageHeaderSection,
+  EuiTitle,
+} from "@elastic/eui"
+import { RegistrationForm } from "../../components"
+import styled from "styled-components"
+
+const StyledEuiPage = styled(EuiPage)`
+  flex: 1;
+`
+const StyledEuiPageHeader = styled(EuiPageHeader)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 2rem;
+
+  & h1 {
+    font-size: 3.5rem;
+  }
+`
 
 export default function RegistrationPage() {
-  return <h1>Registration</h1>
+  return (
+    <StyledEuiPage>
+      <EuiPageBody component="section">
+        <StyledEuiPageHeader>
+          <EuiPageHeaderSection>
+            <EuiTitle size="l">
+              <h1>Registration</h1>
+            </EuiTitle>
+          </EuiPageHeaderSection>
+        </StyledEuiPageHeader>
+        <EuiPageContent verticalPosition="center" horizontalPosition="center">
+          <EuiPageContentBody>
+            <RegistrationForm />
+          </EuiPageContentBody>
+        </EuiPageContent>
+      </EuiPageBody>
+    </StyledEuiPage>
+  )
 }
+
